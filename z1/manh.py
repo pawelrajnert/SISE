8bfa17570eba4db434daf65ea1ada3db66423845
Paweł Rajnert <pawelr6@onet.pl>
2025-04-22 20:12:22 +0200

    Manhattan update

--- z1/manh.py
+++ z1/manh.py
@@ -1,17 +1,18 @@
-from additionalMethods import *
-
 # w tej metryce patrzymy jaka jest odległość bloczka od jego miejsca docelowego
 def manhattan(currentState):
     distance = 0
-    goalBoard = prepareGoalBoard(currentState)
+    rowAmount = currentState.rowSize
+    colAmount = currentState.colSize
 
-    for i in range(0, currentState.RowSize):
-        for j in range(0, currentState.ColumnSize):
+
+    for i in range(0, rowAmount):
+        for j in range(0, colAmount):
             if (currentState.state[i][j] == 0): # zgodnie z informacją na zajęciach, ignorujemy bloczek 0 aby nie przeszacować wyniku
                 continue
 
-            #xDistance = abs(goalBoard[i][j] - currentState.state[i][j])
-            #yDistance = abs(goalBoard[j][i] - currentState.state[j][i])
-            #distance += xDistance + yDistance
+            row = int((currentState.state[i][j]-1) / rowAmount)
+            col = (currentState.state[i][j]-1) % colAmount
+
+            distance += abs(row - i) + abs(col - j)
 
     return distance
